9b6844b0795561713df8f50724bdd26796c24f87
Megan Kaufman <mlkaufman14@gmail.com>
2022-02-10 20:18:49 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      locations: [
+        {
+          id: 1,
+          name: 'Phoenix'
+        },
+        {
+          id: 2,
+          name: 'Tulum'
+        },
+        {
+          id: 3,
+          name: 'Punta Cana'
+        }
+      ]
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.locations.map(location => <h1 key={location.id}> {location.name} </h1> )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
